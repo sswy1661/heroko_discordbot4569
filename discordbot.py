873286792727166761660b891ac56f75afd96b5a
sswy1661 <108062507+sswy1661@users.noreply.github.com>
2022-06-23 19:12:01 +0800

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -17,7 +17,24 @@ async def on_ready():
     print('成功登入')
 
 # 收到訊息時呼叫
+@client.event
+async def on_message(message):
+    # 送信者為Bot時無視
+    if message.author.bot:
+        return
 
+    if client.user in message.mentions: # @判定
+        translator = googletrans.Translator()
+        robotName = client.user.name
+        first, space, content = message.clean_content.partition('@'+robotName+' ')
+
+        if content == '':
+            content = first
+        if translator.detect(content).lang == DSTLanguage:
+            return
+        if translator.detect(content).lang == SRCLanguage or SRCLanguage == '':
+            remessage = translator.translate(content, dest='zh-tw').text
+            await message.reply(remessage) 
 @client.event
 #當有訊息時
 async def on_message(message):
